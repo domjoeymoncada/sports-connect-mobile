0408e02abed273c515c74a158dbda9fc3fb8c124
domjoeymoncada <domjoeymoncada@gmail.com>
2023-06-18 04:27:06 +0800

    Fix absolute path error with babel-plugin-root-import

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        rootPathPrefix: '@',
+        rootPathSuffix: '.',
+      },
+    ],
+  ],
+  env: {
+    production: {
+      plugins: [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '@',
+          rootPathSuffix: '.',
+        },
+      ],
+    },
+  },
 };
